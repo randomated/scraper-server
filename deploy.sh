e413d4d96239a02e30cc50ebde33566a19db8133
Argie Bacomo <argie@bpoc.co.jp>
2023-05-23 17:23:49 +0800

    Changed time to cronjob

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ nohup python3 server.py &
 # Check if main.py is already in the cron job
 if ! crontab -l | grep -q "main.py"; then
     # Add the cron job for main.py
-    (crontab -l 2>/dev/null; echo "0 3 * * * python main.py") | crontab -
+    (crontab -l 2>/dev/null; echo "0 1 * * * python main.py") | crontab -
     echo "main.py added to the cron job."
 else
     echo "main.py already exists in the cron job. No changes made."
