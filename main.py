6b951a9f327af5a2193e13aefeaa8d68ed51e1c7
Argie Bacomo <argie@bpoc.co.jp>
2023-05-29 17:50:47 +0800

    Added shiro-shiro scraper and minified main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from scrapers.loft import LoftScraper
 from scrapers.the_melon import TheMelonScraper
 from scrapers.flying_tiger import FlyingTigerScraper
 from scrapers.hay_japan import HayJapanScraper
+from scrapers.shiro_shiro import ShiroShiroScraper
 from database.saver import Saver
 import os
 
@@ -19,6 +20,20 @@ success_logger = Logger('success', current_directory)
 summary_logger = Logger('summary', current_directory)
 
 saver = Saver(current_directory)
+
+# Mapping between JSON types and classes
+class_mapping = {
+  "komeda": KomedaScraper,
+  "bio-c-bon": BioCBonScraper,
+  "crisp": CrispScraper,
+  "masterwal": MasterwalScraper,
+  "in-sea": InSeaScraper,
+  "loft": LoftScraper,
+  "the_melon": TheMelonScraper,
+  "flying_tiger": FlyingTigerScraper,
+  "hay_tokyo": HayJapanScraper,
+  "shiro": ShiroShiroScraper
+}
 
 scrape_list = { 
   "scrape_list": [
@@ -82,149 +97,38 @@ scrape_list = {
         {"store_name": "HAY TOKYO", "wls_id": 219},
       ],  
       "type": "hay_tokyo"
+    },
+    {
+      "stores": [
+        {"store_name": "SHIRO 渋谷ヒカリエ ShinQs店", "wls_id": 278},
+        {"store_name": "SHIRO ＋Q（プラスク）ビューティー渋谷スクランブルスクエア店", "wls_id": 277},
+        {"store_name": "SHIRO BEAUTY 表参道本店", "wls_id": 276},
+        {"store_name": "SHIRO 表参道本店", "wls_id": 275},
+      ],  
+      "type": "shiro"
     }
   ]
 }
 
-
 will_hide = True
 
-# result = [texts, new_link, image_link, h1_text]
 for item in scrape_list["scrape_list"]:
-  if item['type'] == "komeda":
-    scraper = KomedaScraper(failed_logger, success_logger, info_logger, will_hide, False)
-    result = scraper.start()
-    scraper.close()
-    if result['is_success'] is True:
-      summary_logger.log('KOMEDA SUCCESS')
-
-      data = result['data']
-      inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
-
-      for store in item["stores"]:
-        saver.add_store(inserted_id, store['store_name'], store['wls_id'])
-    else:
-      summary_logger.log('KOMEDA FAILED')
-
-  elif item['type'] == "bio-c-bon":
-    scraper = BioCBonScraper(failed_logger, success_logger, info_logger, will_hide, False)
-    result = scraper.start()
-    scraper.close()
-    if result['is_success'] is True:
-      summary_logger.log('BIO-C-BON SUCCESS')
-
-      data = result['data']
-      inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
-
-      for store in item["stores"]:
-        saver.add_store(inserted_id, store['store_name'], store['wls_id'])
-    else:
-      summary_logger.log('BIO-C-BON FAILED')
-
-  elif item['type'] == "crisp":
-    scraper = CrispScraper(failed_logger, success_logger, info_logger, will_hide, False)
-    result = scraper.start()
-    scraper.close()
-    if result['is_success'] is True:
-      summary_logger.log('CRISP SUCCESS')
-
-      data = result['data']
-      inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
-
-      for store in item["stores"]:
-        saver.add_store(inserted_id, store['store_name'], store['wls_id'])
-    else:
-      summary_logger.log('CRISP FAILED')
-
-  elif item['type'] == "masterwal":
-    scraper = MasterwalScraper(failed_logger, success_logger, info_logger, will_hide, False)
-    result = scraper.start()
-    scraper.close()
-    if result['is_success'] is True:
-      summary_logger.log('MASTERWAL SUCCESS')
-
-      data = result['data']
-      inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
-
-      for store in item["stores"]:
-        saver.add_store(inserted_id, store['store_name'], store['wls_id'])
-    else:
-      summary_logger.log('MASTERWAL FAILED')
-
-  elif item['type'] == "in-sea":
-    scraper = InSeaScraper(failed_logger, success_logger, info_logger, will_hide, False)
-    result = scraper.start()
-    scraper.close()
-    if result['is_success'] is True:
-      summary_logger.log('IN-SEA SUCCESS')
-
-      data = result['data']
-      inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
-
-      for store in item["stores"]:
-        saver.add_store(inserted_id, store['store_name'], store['wls_id'])
-    else:
-      summary_logger.log('IN-SEA FAILED')
-
-  elif item['type'] == "loft":
-    scraper = LoftScraper(failed_logger, success_logger, info_logger, will_hide, False)
-    result = scraper.start()
-    scraper.close()
-    if result['is_success'] is True:
-      summary_logger.log('LOFT SUCCESS')
-
-      data = result['data']
-      inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
-
-      for store in item["stores"]:
-        saver.add_store(inserted_id, store['store_name'], store['wls_id'])
-    else:
-      summary_logger.log('LOFT FAILED')
-
-  elif item['type'] == "the_melon":
-    scraper = TheMelonScraper(failed_logger, success_logger, info_logger, will_hide, False)
-    result = scraper.start()
-    scraper.close()
-    if result['is_success'] is True:
-      summary_logger.log('THE-MELON SUCCESS')
-
-      data = result['data']
-      inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
-
-      for store in item["stores"]:
-        saver.add_store(inserted_id, store['store_name'], store['wls_id'])
-    else:
-      summary_logger.log('THE-MELON FAILED')
-
-  elif item['type']  == "flying_tiger":
-    scraper = FlyingTigerScraper(failed_logger, success_logger, info_logger, will_hide, False)
-    result = scraper.start()
-    scraper.close()
-    if result['is_success'] is True:
-      summary_logger.log('FLYING-TIGER SUCCESS')
-
-      data = result['data']
-      inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
-
-      for store in item["stores"]:
-        saver.add_store(inserted_id, store['store_name'], store['wls_id'])
-    else:
-      summary_logger.log('FLYING-TIGER FAILED')
+  cls = class_mapping[item['type']]
+  scraper = cls(failed_logger, success_logger, info_logger, will_hide, False)
+  result = scraper.start()
+  scraper.close()
   
-  elif item['type']  == "hay_tokyo":
-    scraper = HayJapanScraper(failed_logger, success_logger, info_logger, will_hide, False)
-    result = scraper.start()
-    scraper.close()
-    if result['is_success'] is True:
-      summary_logger.log('HAY-JAPAN SUCCESS')
+  if result['is_success'] is True:
+    summary_logger.log(f"{item['type']} SUCCESS")
 
-      data = result['data']
-      inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
+    data = result['data']
+    inserted_id = saver.add_scraped_data(data['title'], data['description'], data['site_url'], data['images'])
 
-      for store in item["stores"]:
-        saver.add_store(inserted_id, store['store_name'], store['wls_id'])
-    else:
-      summary_logger.log('HAY-JAPAN FAILED')
-  
-      
+    for store in item["stores"]:
+      saver.add_store(inserted_id, store['store_name'], store['wls_id'])
+  else:
+    summary_logger.log(f"{item['type']} FAILED")
+
 saver.close_db()
+
+
