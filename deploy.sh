0a0a0bb5fd74bbfa0351ed36ca213ddefbdabd3d
Argie Bacomo <argie@bpoc.co.jp>
2023-06-02 14:52:11 +0800

    Changed instructions and deploy setup

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-nohup python3.11 server.py &
+nohup python3 server.py &
 # Check if main.py is already in the cron job
 if ! crontab -l | grep -q "main.py"; then
-    (crontab -l 2>/dev/null; echo "0 0 * * * python3.11 /home/mrink/scraper-server/main.py") | crontab -
+    (crontab -l 2>/dev/null; echo "0 0 * * * python3 /home/mrink/scraper-server/main.py") | crontab -
     echo "main.py added to the cron job."
 else
     echo "main.py already exists in the cron job. No changes made."
