d5f9c7323a13639e6ab85646d1b09eeee900aa1e
Argie Bacomo <argie@bpoc.co.jp>
2023-06-07 17:17:25 +0800

    Changed main.py

--- generator.py
+++ generator.py
@@ -1,30 +1,18 @@
 import json
 text = """
-ハンズ渋谷スクランブルスクエア店	https://hands.net/	206
-ハンズ渋谷店	https://hands.net/	205
-ハンズ新宿店	https://hands.net/	204
-WithGreen恵比寿店	https://withgreen.club/	390
-WithGreen渋谷ヒカリエShinQs東横のれん街店	https://withgreen.club/	389
-GU渋谷店	https://www.gu-global.com/jp/ja/feature/list/pc/	181
-nergy ATRE EBISU	https://www.nergy.jp/	165
-nergy SHIBUYA SCRAMBLE SQUARE	https://www.nergy.jp/	164
-FIGHTING ROAD東京店	https://www.fightingroad.co.jp/	196
-MIZUNO TOKYO	https://jpn.mizuno.com/campaign?did=dtctop_campaignlist	197
-2foods 渋⾕ロフト店	https://2foods.jp/	419
-ガーデンラウンジ「坐忘」	https://www.tokyuhotels.co.jp/cerulean-h/restaurant/zabou/index.html	484
-ザ・レギャン・トーキョー	https://www.legian.jp/	486
-Dining TORRENT	https://www.tokyuhotels.co.jp/stream-e/bar_dining/index.html	491
-LUSH 原宿店	https://weare.lush.com/jp/	283
-LUSH 渋谷駅前店	https://weare.lush.com/jp/	282
-ヨガスタジオ YOGIWAY	https://www.yogiway.jp/	258
-BRUNO 渋谷ヒカリエ ShinQs	https://bruno-onlineshop.com/	401
-Shibuya-san	https://shibuya-san.co.jp/	646
-渋谷区立松濤美術館	https://shoto-museum.jp/	650
-8/(ヒカリエ)	http://www.hikarie8.com/home.shtml	656
-Seria代官山アドレス・ディセ店	https://www.seria-group.com/	223
-AKOMEYA TOKYO 東急プラザ渋谷	https://www.akomeya.jp/shop/pc/0feature/	218
-Sushi Gonpachi Shibuya	https://gonpachi.jp/news/	437
-Gonpachi Shibuya	https://gonpachi.jp/news/	436
+GOLD'S GYM代々木公園PREMIUM	https://www.goldsgym.jp/	643
+GOLD'S GYM代々木上原東京	https://www.goldsgym.jp/	642
+アディダス オリジナルス ガールズ ショップ 渋谷109	https://shop.adidas.jp/	171
+アディダス オリジナルス フラッグシップ ストア 原宿	https://shop.adidas.jp/	170
+アディダス ブランドセンター原宿	https://shop.adidas.jp/	169
+アディダス ブランドセンター RAYARD MIYASHITA PARK	https://shop.adidas.jp/	168
+アディダス ブランドセンター 渋谷	https://shop.adidas.jp/	167
+イージーヨガストア代官山	https://www.easyoga.jp/	166
+アリーナショップ 東京	https://store.descente.co.jp/arena/	192
+DESCENTE TOKYO	https://store.descente.co.jp/descente/	194
+よしもと∞ホール	https://mugendai.yoshimoto.co.jp/	644
+ヴィレッジヴァンガード渋谷本店	https://www.village-v.co.jp/	203
+DESCENT GOLF 新宿タカシマヤ	https://store.descente.co.jp/descentegolf/ext/news/	195
 """
 
 scrape_list = { "scrape_list": [] }
